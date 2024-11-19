1e4f7a112675f4805b1c02eddedb297f77214db0
Tamir Bahar <tamir.bahar@gmail.com>
2024-11-19 15:05:05 +0200

    Minor tweaks

--- cmpgen.go
+++ cmpgen.go
@@ -18,10 +18,14 @@ func newRegistryKey[T any](fields ...string) registryKey {
 	return registryKey{Type: reflect.TypeOf(*new(T)), Fields: strings.Join(fields, ", ")}
 }
 
+// Register is meant to be used in the generated code only.
 func Register[T any](fn any, fields ...string) {
 	registry[newRegistryKey[T](fields...)] = fn
 }
 
+// CmpByFields provides a cmp function to be used with sorting functions.
+// The type parameter is the type to compare, and the fields are the names
+// of the fields to compare.
 func CmpByFields[T any](field string, fields ...string) func(T, T) int {
 	regKey := newRegistryKey[T](slices.Insert(fields, 0, field)...)
 	if regFunc, ok := registry[regKey]; ok {
